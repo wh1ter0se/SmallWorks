88627c8100a5e27e4867306a8f64fe8a431cefaa
wh1ter0se <rckreischer@gmail.com>
2022-09-19 13:40:47 -0400

    Got (uncontrolled) double pendulum working.

--- InvertedPendulum/pymunk_V1-0/double_pend.py
+++ InvertedPendulum/pymunk_V1-0/double_pend.py
@@ -8,15 +8,26 @@ class PinJoint:
         joint = pymunk.constraint.PinJoint(b, b2, a, a2)
         space.add(joint)
 
+class Circle:
+    def __init__(self, pos, radius=20):
+        self.body = pymunk.Body()
+        self.body.position = pos
+        shape = pymunk.Circle(self.body, radius)
+        shape.density = 0.01
+        shape.friction = 0.5
+        shape.elasticity = 1
+        space.add(self.body, shape)
+
+
 b0 = space.static_body
 
 p = pymunk.Vec2d(200, 190 )
 v = pymunk.Vec2d(80, 0)
 
-c = pymunk.Circle(p+v)
-PinJoint(b0, c.body, p)
+c1 = Circle(p+v)
+PinJoint(b0, c1.body, p)
 
 c2 = pymunk.Circle(p+2*v)
-PinJoint(c.body, c2.body)
+PinJoint(c1.body, c2.body)
 
 pymunk.App().run()
